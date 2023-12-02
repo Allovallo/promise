e738e61e22b46937e854dfa779ec0440e0d8eea8
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-12-02 10:17:14 +0200

    Files updated

--- src/js/03-racetrack.js
+++ src/js/03-racetrack.js
@@ -23,7 +23,7 @@ function onStart() {
   raceCounter += 1;
   const promises = horses.map(run);
 
-  undateWinnerField('');
+  updateWinnerField('');
   updateProgressField('Заїзд розпочався, ставки не приймаються!');
   determineWinner(promises);
   waitForAll(promises);
@@ -31,18 +31,18 @@ function onStart() {
 
 function determineWinner(horsesP) {
   Promise.race(horsesP).then(({ horse, time }) => {
-    undateWinnerField(`Переміг ${horse}, який фінішував за ${time} часу!!!`);
+    updateWinnerField(`Переміг ${horse}, який фінішував за ${time} часу!!!`);
     updateResultsTable({ horse, time, raceCounter });
   });
 }
 
 function waitForAll(horsesP) {
   Promise.all(horsesP).then(() => {
-    updateProgressField('Заїзд закінчено, ставки приймаються!');
+    updateProgressField('Заїзд закінчився, ставки приймаються!');
   });
 }
 
-function undateWinnerField(message) {
+function updateWinnerField(message) {
   refs.winnerField.textContent = message;
 }
 
@@ -51,14 +51,11 @@ function updateProgressField(message) {
 }
 
 function updateResultsTable({ horse, time, raceCounter }) {
-  const tr = `<tr><td>${raceCounter}</td><td>${horse}</td><td>${time}</td></tr>`;
+  const tr = `<tr><td>${raceCounter}</td><td>${horse}</td><td>${time}</td><tr/>`;
   refs.tableBody.insertAdjacentHTML('beforeend', tr);
 }
 
-// console.log(
-//   '%c Заїзд розпочався, ставки не приймаються!',
-//   'color: brown; font-size: 14px',
-// );
+const promises = horses.map(run);
 
 function run(horse) {
   return new Promise(resolve => {
@@ -73,18 +70,3 @@ function run(horse) {
 function getRandomTime(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-// console.log(
-//   '%c Заїзд розпочався, ставки не приймаються!',
-//   'color: brown; font-size: 14px',
-// );
-
-// console.log(
-//   `%c Переміг ${1}, який фінішував за ${1} часу!!!`,
-//   'color: green; font-size: 14px;',
-// );
-
-// console.log(
-//   '%c Заїзд закінчено, ставки приймаються!',
-//   'color: blue; font-size: 14px',
-// );
